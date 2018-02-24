9d8880019153d4793d024481a61fd6b14a9d8c33
Giovani Paganini <giovanipaganini@outlook.com>
2018-02-23 22:25:02 -0300

    for

--- index.php
+++ index.php
@@ -11,8 +11,22 @@
 
 	<?php
 		$texto = '<p>huehuehue</p>';
+		$texto2 = '<p>disgrasa</p>';
 		$i = 1;
+
 		while ($i <= 10) {
 				echo $texto;
 				$i++;
 		}
+
+		for ($i = 1; $i <= 10; $i++) {
+			echo $texto;
+
+		}
+
+
+	?>
+
+
+</body>
+</html>
