8880fb533ad6204282ef36f1072687c359c31241
Giovani Paganini <giovanipaganini@outlook.com>
2018-02-23 22:10:08 -0300

    while

--- index.php
+++ index.php
@@ -4,29 +4,16 @@
 	<title>Welcome m8</title>
 </head>
 <body>
-	<?php 
-		
 
-		$n1 = 6;
-		$n2 = 6;
-		$n3 = 6;
-		
-		$totalNota = 10;
-
-		$media = ($n1 + $n2 + $n3) / 4;
-		$notaFinal = $media - $totalNota;
-
-		echo $media;
-		echo $notaFinal;
-	?>
 
 	<br>
 	<br>
 
 	<?php
+		$texto = "huehuehue";
 		$i = 1;
-		while ($i <= 10){
-				echo "HUE HUE HUE";
+		while ($i <= 10) {
+				echo $texto;
 				$i++;
 		}
 </body>
